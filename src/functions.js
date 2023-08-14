7562f87dff555c3889f5ef5c265c7017a21f1e0c
vlad.bayandin@list.ru <vlad.bayandin@list.ru>
2023-08-14 05:28:02 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,5 @@
-var OPENWEATHERMAP_API_KEY = $injector.api_key;
-
 function openWeatherMapCurrent(units, lang, q){
+    var OPENWEATHERMAP_API_KEY = $env.get('api_key');
 return $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&units=${units}&lang=${lang}&q=${q}", {
         timeout: 10000,
         query:{
